2020bb867a7c099f8516f1712091dcced1f02f8d
raptalex <raptis@mail.nmc.edu>
2023-02-27 16:51:56 -0500

    Slight modification

--- ClubInterface/Program.cs
+++ ClubInterface/Program.cs
@@ -11,42 +11,41 @@ interface IClub
     public string Fullname();
 }
 
-class ClubMember : IClub
-{
-    public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    // Age (in years)
-    public double age { get; set; }
-    // Weight (in US pounds)
-    public double weight { get; set; }
-    // Height (in feet)
-    public double height { get; set; }
-
-    public ClubMember()
-    {
-        Id = 0;
-        FirstName = "none";
-        LastName = "none";
-        age = weight = height = 0;
-    }
-    public ClubMember(int _id, string _fname, string _lname, double _age, double _weight, double _height)
-    {
-        Id = _id;
-        FirstName = _fname;
-        LastName = _lname;
-        age = _age;
-        weight = _weight;
-        height = _height;
-    }
-    public string Fullname()
-    {
-        return FirstName + " " + LastName;
-    }
-}
-
 class Program
 {
+    class ClubMember : IClub
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        // Age (in years)
+        public double age { get; set; }
+        // Weight (in US pounds)
+        public double weight { get; set; }
+        // Height (in feet)
+        public double height { get; set; }
+
+        public ClubMember()
+        {
+            Id = 0;
+            FirstName = "none";
+            LastName = "none";
+            age = weight = height = 0;
+        }
+        public ClubMember(int _id, string _fname, string _lname, double _age, double _weight, double _height)
+        {
+            Id = _id;
+            FirstName = _fname;
+            LastName = _lname;
+            age = _age;
+            weight = _weight;
+            height = _height;
+        }
+        public string Fullname()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
     static public void Main(string[] args)
     {
         ClubMember member1 = new ClubMember(89, "Michael", "Rodriguez", 23, 159.45, 5.3);
